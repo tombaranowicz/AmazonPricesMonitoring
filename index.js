5f3070f597dbc104963ce1eb27c719504f0fd5bf
Subhendu Swain <suvendu.swain@gmail.com>
2021-09-04 13:04:51 +1000

    Fix $ is not an function due to cheerio upgrade

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const puppeteer = require('puppeteer');
-const $ = require('cheerio');
+const $ = require('cheerio').load('');
 const CronJob = require('cron').CronJob;
 const nodemailer = require('nodemailer');
 
